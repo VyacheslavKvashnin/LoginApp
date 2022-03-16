2cb86ac110210304ed77fea33fbb5a0eb014baaa
komplektan <54449303+komplektan@users.noreply.github.com>
2022-03-16 17:53:35 +0500

    Create LoginViewController and WelcomViewController

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  LoginApp
 //
 //  Created by Вячеслав Квашнин on 15.03.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Вячеслав Квашнин on 16.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
