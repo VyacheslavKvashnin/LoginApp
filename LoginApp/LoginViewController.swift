9e0d29d04e445e20490e0cd9915186b5decdb92c
komplektan <54449303+komplektan@users.noreply.github.com>
2022-03-18 19:46:21 +0500

    Changed Storyboard and LoginViewController

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: - IBOutlets
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    let user = "User"
-    let password = "Password"
+    // MARK: - Private Properties
+    private let user = "User"
+    private let password = "Password"
     
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,25 +38,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         welcomeVC.loginLabel = loginTextField.text
     }
     
+    // MARK: - IBActions
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
-        guard unwindSegue.source is WelcomeViewController else { return }
         loginTextField.text = ""
         passwordTextField.text = ""
-        
     }
     
     @IBAction func loginButton() {
         
-        guard let loginTextField = loginTextField.text,
-              !loginTextField.isEmpty,
-              loginTextField == user
+        guard loginTextField.text == user
         else {
             showAlert(with: "Invalid login or password", and: "Enter the correct username and password")
             return
         }
-        guard let passwordTextField = passwordTextField.text,
-              !passwordTextField.isEmpty,
-              passwordTextField == password
+        guard passwordTextField.text == password
         else {
             showAlert(with: "Invalid login or password", and: "Enter the correct username and password")
             return
@@ -69,6 +67,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         showAlert(with: "Oops!", and: "Your password is \(password)")
     }
     
+    // MARK: - Public Methods
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         if textField == loginTextField {
@@ -109,6 +108,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    // MARK: - Private Methods
     private func showAlert(with title: String, and message: String) {
         
         let alert = UIAlertController(
